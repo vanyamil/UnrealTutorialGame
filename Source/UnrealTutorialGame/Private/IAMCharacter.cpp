b6cd3647a90af86a1ab9522b0e047bd49d68a112
Ivan Miloslavov <miloslavovivan@gmail.com>
2022-06-16 12:06:23 -0400

    A2.1 - Projectile shoots towards crosshair

--- Source/UnrealTutorialGame/Private/IAMCharacter.cpp
+++ Source/UnrealTutorialGame/Private/IAMCharacter.cpp
@@ -7,6 +7,8 @@
 #include <GameFramework/SpringArmComponent.h>
 #include <Camera/CameraComponent.h>
 #include <GameFramework/CharacterMovementComponent.h>
+#include <Kismet/KismetMathLibrary.h>
+#include <DrawDebugHelpers.h>
 
 // Sets default values
 AIAMCharacter::AIAMCharacter()
@@ -40,6 +42,21 @@ void AIAMCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// -- Rotation Visualization -- //
+	const float DrawScale = 100.0f;
+	const float Thickness = 5.0f;
+
+	FVector LineStart = GetActorLocation();
+	// Offset to the right of pawn
+	LineStart += GetActorRightVector() * 100.0f;
+	// Set line end in direction of the actor's forward
+	FVector ActorDirection_LineEnd = LineStart + (GetActorForwardVector() * 100.0f);
+	// Draw Actor's Direction
+	DrawDebugDirectionalArrow(GetWorld(), LineStart, ActorDirection_LineEnd, DrawScale, FColor::Yellow, false, 0.0f, 0, Thickness);
+
+	FVector ControllerDirection_LineEnd = LineStart + (GetControlRotation().Vector() * 100.0f);
+	// Draw 'Controller' Rotation ('PlayerController' that 'possessed' this character)
+	DrawDebugDirectionalArrow(GetWorld(), LineStart, ControllerDirection_LineEnd, DrawScale, FColor::Green, false, 0.0f, 0, Thickness);
 }
 
 // Called to bind functionality to input
@@ -88,14 +105,29 @@ void AIAMCharacter::PrimaryAttack()
 
 void AIAMCharacter::PrimaryAttack_TimeElapsed()
 {
-	FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
+	if(ensure(ProjectileClass))
+	{
+		FVector HandLocation = GetMesh()->GetSocketLocation("Muzzle_01");
 
-	FTransform SpawnTM{ GetControlRotation(), HandLocation };
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	SpawnParams.Instigator = this;
+		// Line trace to expected hit location
+		AActor* MyOwner = GetOwner();
+		FVector EyeLocation = CameraComp->GetComponentLocation();
+		FVector EyeEnd = EyeLocation + CameraComp->GetForwardVector() * 4000.f; // randomly picked distance
 
-	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+		FHitResult Hit;
+		bool foundHit = GetWorld()->LineTraceSingleByObjectType(Hit, EyeLocation, EyeEnd, { FCollisionObjectQueryParams::InitType::AllObjects });
+
+		// Find direction to point to aim there
+		FVector AimAt = foundHit ? Hit.ImpactPoint : EyeEnd;
+		FRotator AimAtRotation = UKismetMathLibrary::FindLookAtRotation(HandLocation, AimAt);
+
+		FTransform SpawnTM(AimAtRotation, HandLocation);
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		SpawnParams.Instigator = this;
+
+		GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+	}
 }
 
 void AIAMCharacter::PrimaryInteract()
